b315f208d03181c3229b451090ee50c7636c15ad
Petro Smoliar <smp22pl@gmail.com>
2023-10-22 14:08:59 +0200

    impl Book, BookRepository, BookService

--- src/main/java/com/example/book/store/BookStoreApplication.java
+++ src/main/java/com/example/book/store/BookStoreApplication.java
@@ -1,11 +1,32 @@
 package com.example.book.store;
 
+import com.example.book.store.model.Book;
+import com.example.book.store.service.BookService;
+import java.math.BigDecimal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookStoreApplication {
+    @Autowired
+    private BookService bookService;
+
     public static void main(String[] args) {
         SpringApplication.run(BookStoreApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            Book newBook = new Book();
+            newBook.setTitle("java");
+            newBook.setPrice(BigDecimal.valueOf(54));
+            newBook.setAuthor("mate");
+            newBook.setIsbn("ssdd7788");
+            bookService.save(newBook);
+        };
     }
 }
--- src/test/java/com/example/book/store/BookStoreApplicationTests.java
+++ src/test/java/com/example/book/store/BookStoreApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.book.store;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BookStoreApplicationTests.class)
 class BookStoreApplicationTests {
-
+    @Test
+    void contextLoads() {
+    }
 }
