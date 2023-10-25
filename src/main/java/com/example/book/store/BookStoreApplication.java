a6de5d4477f3dbf885ae03ee7daeb94561e106ba
Petro Smoliar <smp22pl@gmail.com>
2023-10-25 13:36:53 +0200

    imp BookDto.class, CreateBookRequestDto.class,Mapstruct and mappers

--- src/main/java/com/example/book/store/BookStoreApplication.java
+++ src/main/java/com/example/book/store/BookStoreApplication.java
@@ -1,14 +1,10 @@
 package com.example.book.store;
 
 import com.example.book.store.mapper.BookMapper;
-import com.example.book.store.model.Book;
 import com.example.book.store.repository.BookRepository;
-import java.math.BigDecimal;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookStoreApplication {
