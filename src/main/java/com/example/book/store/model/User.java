024c8eb831a37ffd7a1c53552dcfa221d86202d4
Petro Smoliar <smp22pl@gmail.com>
2023-11-30 10:59:29 +0100

    Added the following method to the AuthenticationController: public UserLoginResponseDto login(UserLoginRequestDto request) Added AuthenticationService, JwtAuthenticationFilter, JwtUtil classes

--- src/main/java/com/example/book/store/model/User.java
+++ src/main/java/com/example/book/store/model/User.java
@@ -38,7 +38,7 @@ public class User implements UserDetails {
     private String lastName;
     @Column(name = "shipping_addresses")
     private String shippingAddress;
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(
             name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
--- src/main/java/com/example/book/store/repository/user/UserRepository.java
+++ src/main/java/com/example/book/store/repository/user/UserRepository.java
@@ -2,8 +2,10 @@ package com.example.book.store.repository.user;
 
 import com.example.book.store.model.User;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = "roles")
     Optional<User> findByEmail(String email);
 }
